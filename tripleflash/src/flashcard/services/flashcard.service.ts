205931b9026143134562db4b80c7c62f674405fb
Matias <schakelmatias@gmail.com>
2020-02-01 14:38:41 +0900

    Added json list and parsing logic

--- tripleflash/src/flashcard/services/flashcard.service.ts
+++ tripleflash/src/flashcard/services/flashcard.service.ts
@@ -1,19 +1,24 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { jsonList } from '../constants/jsonList';
 
 @Injectable({
   providedIn: 'root',
 })
 export class FlashCardService {
+  fullList: Array<TripleFlashCard>;
 
-  constructor() { }
+  constructor() {
+    this.fullList = parseJsonList();
+  }
 
   private mockGetCardSet(): Observable<Array<TripleFlashCard>> {
     return of(mockSet);
   }
 
   public getFlashCardSet(): Observable<Array<TripleFlashCard>> {
-    return this.mockGetCardSet();
+    // return this.mockGetCardSet();
+    return of(this.fullList);
   }
 
 }
@@ -30,3 +35,16 @@ const mockSet = [
     {kanji: 'mizu kanji', hiragana: 'mizu hiragana', english: 'water' },
     {kanji: '試験', hiragana: 'テスト', english: 'water' }
 ];
+
+function parseJsonList(): Array<TripleFlashCard> {
+  const list: Array<Array<string>> = JSON.parse(jsonList);
+  const parsedSet: Array<TripleFlashCard> = [];
+  list.forEach((cardArray) => {
+    if (cardArray.length == 2) {
+      parsedSet.push({kanji: 'No Kanji Available', hiragana: cardArray[0], english: cardArray[1] });
+    } else if (cardArray.length == 3) {
+      parsedSet.push({kanji: cardArray[0], hiragana: cardArray[1], english: cardArray[2] });
+    }
+  });
+  return parsedSet;
+}
